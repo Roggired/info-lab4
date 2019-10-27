f06164f0a5cf4b33cb30fc5742ea47662ed9e9a6
Alexey <leshaego14@gmail.com>
2019-10-27 21:56:14 +0300

    Added work with libs.

--- Lab4.py
+++ Lab4.py
@@ -2,6 +2,10 @@ from jsonParser.SyntaxException import SyntaxException
 from jsonParser.JSONParser import JSONParser
 from yamlConverter.YAMLConverter import YAMLConverter
 
+import json
+import yaml
+from time import time
+
 
 def check_root_braces(string):
     if string[0] == '{' and string[len(string) - 1] == '}':
@@ -14,8 +18,7 @@ def remove_root_braces(string):
     return string[1: len(string) - 1]
 
 
-if __name__ == "__main__":
-    text = open("schedules\\example.json", encoding='UTF-8').read()
+def use_own_parser_and_converter(text):
     text = text.replace(" ", "")
     text = text.replace("\n", "")
 
@@ -24,10 +27,38 @@ if __name__ == "__main__":
 
     text = remove_root_braces(text)
 
-    jsonParser = JSONParser(text)
-    entity_set = jsonParser.parse()
+    json_parser = JSONParser(text)
+    entity_set = json_parser.parse()
 
-    yamlConverter = YAMLConverter(entity_set)
-    result = yamlConverter.convert()
+    yaml_converter = YAMLConverter(entity_set)
+    return yaml_converter.convert()
 
-    open("output\\example.yaml", mode='w', encoding='UTF-8').write(result)
+
+def use_libs(text, output_file):
+    jsons_data = json.loads(text)
+    yaml.dump(jsons_data, output_file, encoding='UTF-8', allow_unicode=True)
+
+
+if __name__ == "__main__":
+    mode = input()
+
+    start_time = time()
+    for counter in range(10):
+        input_file = open("schedules\\example.json", encoding='UTF-8')
+        text = input_file.read()
+        input_file.close()
+
+        output_file = open("output\\example.yaml", mode='w', encoding='UTF-8')
+
+        if mode == '0':
+            use_libs(text, output_file)
+
+        if mode == '1':
+            output_file.write(use_own_parser_and_converter(text))
+
+        output_file.close()
+
+    end_time = time()
+
+    delta_time = end_time - start_time
+    print("Время выполнения: " + str(delta_time))
